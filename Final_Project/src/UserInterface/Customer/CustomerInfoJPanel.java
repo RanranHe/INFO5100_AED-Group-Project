cd0a042f6b1a5bec472536d65f39fe533b609150
Ranran He <he.ra@husky.neu.edu>
2018-11-20 04:02:30 -0500

    update package name

--- Final_Project/src/UserInterface/Customer/CustomerInfoJPanel.java
+++ Final_Project/src/UserInterface/Customer/CustomerInfoJPanel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Interface.Customer;
+package UserInterface.Customer;
 
 import Business.UserAccount.CustomerAccount;
 import Business.UserAccount.UserAccount;
--- Final_Project/src/UserInterface/Customer/CustomerRegistrationInfoJPanel.java
+++ Final_Project/src/UserInterface/Customer/CustomerRegistrationInfoJPanel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Interface.Customer;
+package UserInterface.Customer;
 
 import Business.Customer.Customer;
 import Business.DB4OUtil.DB4OUtil;
